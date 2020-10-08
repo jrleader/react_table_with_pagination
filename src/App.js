f4e6d9bb2ee8d962d4554f86dd6fb95cd67b826b
Xiaoming Li <lixiaoming1993@gmail.com>
2020-10-08 23:59:54 +0800

    Try to use react-router for page navigation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
 import Table from "./table.js"
-import React, { Component } from "react";
+import React, { Component } from "react"
 import TopMenu from "./topmenu.js"
+// import ReactRouter from "react-router"
+import CommonContent from "./commonLayout"
+import AboutPage from "./aboutPage"
+import {BrowserRouter as Router, Route,Link} from "react-router-dom"
 
-const USERS_URL = "mocked_data/gym_members.json";
+// let ReactRouter = require('react-router-dom')
+// let {Route, Link, BrowserRouter} = ReactRouter
+// let Router = BrowserRouter
+// const {hashHistory} = ReactRouter
+
+// let ReactRouter = require('react-router')
+// let {Router, Route} = ReactRouter
+
+const history = require('history')
+
+// let hashHistory = ReactRouter.useHistory(history.createHashHistory) (
+//     {
+//         queryKey: false
+//     }
+// )
+
+let hashHistory = history.createHashHistory()
+
+const USERS_URL = "../mocked_data/gym_members.json"
+// const USERS_URL = "D:\\lixia\\GitHubProjects\\react_table_with_pagination\\public\\mocked_data\\gym_members.json"
 
 class App extends Component {
+
+
     render() {
         // const divStyle = {
         //     display: flex,
@@ -16,14 +41,41 @@ class App extends Component {
 
         }
 
+        const table = () => {
+            return (
+                <Table data-url={USERS_URL}/>
+            )
+        }
+
         return (
             // <div class="box" style={divStyle}>
             <div>
-                <TopMenu/>
-                <div class="box">
-                    <Table data-url={USERS_URL} itemsPerPage={5} />
+                <Router history={hashHistory}>
+                <nav>
+                    <Link to="/table">Table</Link>
+                    <Link to="/about">About</Link>
+                </nav>
+                <div>
+                    {/* Data loaded however no content is shown in the table, why? */}
+                    <Route path="/table" component={() => (<Table data-url={USERS_URL}/>)}/> 
+                    {/* <Route path="/table" render={(USERS_URL) => (<Table data-url={USERS_URL}/)}/> */}
+                    <Route path="/about" component={AboutPage}/>
                 </div>
+                </Router>
             </div>
+
+            // <Router history={hashHistory}>
+            //     <Route path="/">
+            //         <Route path="/table" component={Table}/>
+            //         <Route path="/about" component={AboutPage}/>
+            //     </Route>
+            // </Router>
+            // <div>
+            //     <TopMenu/>
+            //     <div class="box">
+            //         <Table data-url={USERS_URL} itemsPerPage={5} />
+            //     </div>
+            // </div>
         );
     }
 }
